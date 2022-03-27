4329e8c55c2e345b339c34d5c75e1bd036357c2b
Enoch <Enoragon@gmail.com>
2022-03-27 15:39:49 -0500

    Add spotify url info and update nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import 'express-async-errors';
+import spotifyFactory from 'spotify-url-info';
+import fetch from 'cross-fetch';
 import { SERVER_PORT } from './config';
+
+const spotify = (spotifyFactory as any)(fetch) as typeof spotifyFactory
 
 const app = express();
 
 app.get('/', async (req, res) => {
-    res.json({ test: 'hello' });
+
+    const data = await spotify.getData('https://open.spotify.com/track/5nTtCOCds6I0PHMNtqelas')
+    
+    res.json(data);
 });
 
 app.listen(SERVER_PORT, function () {
