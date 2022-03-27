3611b31e1200f47d4952e783db3dbefa6292469d
Levi Zitting <me@levizitting.com>
2022-03-27 15:46:06 -0500

    fix linting issues

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import spotifyFactory from 'spotify-url-info';
 import fetch from 'cross-fetch';
 import { SERVER_PORT } from './config';
 
-const spotify = (spotifyFactory as any)(fetch) as typeof spotifyFactory
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const spotify = (spotifyFactory as any)(fetch) as typeof spotifyFactory;
 
 const app = express();
 
 app.get('/', async (req, res) => {
+    const data = await spotify.getData('https://open.spotify.com/track/5nTtCOCds6I0PHMNtqelas');
 
-    const data = await spotify.getData('https://open.spotify.com/track/5nTtCOCds6I0PHMNtqelas')
-    
     res.json(data);
 });
 
